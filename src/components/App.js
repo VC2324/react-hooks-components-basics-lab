893a121234e0907baf542555b777a4cced9189f5
Victor <victorac2324@gmail.com>
2024-03-31 22:19:55 -0400

    practiced placeing and writing components in React

--- src/components/App.js
+++ src/components/App.js
@@ -15,16 +15,25 @@ function Home() {
     </div>
   );
 }
+function About (){
+  return (
+    <div id ="about" >
+      <h2>
+        about
+      </h2>
 
-{/* write an <About> component here */}
+    </div>
+  )
+
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      {<About/>}
     </div>
   );
 }
